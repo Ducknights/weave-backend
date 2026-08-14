807989ea0bcdd671a8ffa5213c3dc3f7400be0dd
野生的鸭子 <2897662424@qq.com>
2026-08-14 10:30:28 +0800

    refactor(captcha-service): 修改为非Web应用启动方式

--- services/captcha-service/src/main/java/org/weave/captcha/CaptchaMainApplication.java
+++ services/captcha-service/src/main/java/org/weave/captcha/CaptchaMainApplication.java
@@ -1,11 +1,14 @@
 package org.weave.captcha;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CaptchaMainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CaptchaMainApplication.class, args);
+        new SpringApplicationBuilder(CaptchaMainApplication.class)
+                .web(WebApplicationType.NONE) // 关键：明确指定为非Web应用
+                .run(args);
     }
 }
